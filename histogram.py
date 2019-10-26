63fee500cac75b539c51eb95448ccd1736d391bf
David Evans <evansdav.a@gmail.com>
2019-10-25 17:42:19 -0700

    Added alternate histogram structures

--- histogram.py
+++ histogram.py
@@ -1,14 +1,47 @@
 from dictionary_words import read_file_words
+from utility import time_it
 
 
+@time_it
 def histogram(text):
     """Read a text and return a histogram of its words"""
+    # Dictionary
     hist = {}
     for word in text:
+        word = word.lower()
         if word not in hist:
             hist[word] = 1
         else:
             hist[word] += 1
+
+    # List of lists
+    # hist = []
+    # added = True
+    # for word in text:
+    #     word = word.lower()
+    #     for word_list in hist:
+    #         if word_list[0] == word:
+    #             word_list[1] += 1
+    #             added = False
+    #     if added is True:
+    #         hist.append([word, 1])
+    #     added = True
+
+    # List of tuples
+    # hist = []
+    # added = True
+    # for word in text:
+    #     word = word.lower()
+    #     for word_tup in hist:
+    #         if word_tup[0] == word:
+    #             word_list = list(word_tup)
+    #             word_list[1] += 1
+    #             hist[hist.index(word_tup)] = tuple(word_list)
+    #             added = False
+    #     if added is True:
+    #         hist.append((word, 1))
+    #     added = True
+
     return hist
 
 
@@ -32,4 +65,5 @@ def frequency(histogram, word):
 
 if __name__ == '__main__':
     text = read_file_words('Iliad.txt')
-    print(frequency(histogram(text), 'Apollo'))
+    text = "One fish two fish red fish blue fish".split()
+    print(histogram(text))
