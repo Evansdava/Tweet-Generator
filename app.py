cbd7d5fe120092f4cd315fc86bf3e98264cb1744
David Evans <evansdav.a@gmail.com>
2019-11-04 13:35:04 -0800

    Added title, button functionality, variance

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from python.histogram import run_file
+from random import randint
 
 app = Flask(__name__)
 
@@ -9,6 +10,6 @@ def index():
     if request.args.get('num'):
         num = int(request.args.get('num'))
     else:
-        num = 10
+        num = randint(5, 20)
     sent = run_file(num)
     return render_template('index.html', sent=sent)
